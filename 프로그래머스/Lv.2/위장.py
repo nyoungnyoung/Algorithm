303e5fae14cf274c4a053d4420da17484906cdd7
sunysa8 <sunysa8@naver.com>
2023-02-23 01:33:03 +0900

    프로그래머스-Lv.1 폰켓몬/Lv.2 위장

--- 프로그래머스/Lv.2/위장.py
+++ 프로그래머스/Lv.2/위장.py
@@ -3,12 +3,10 @@ def solution(clothes):
     cloth = {}
     for i in clothes:
         key, value = i[1], i[0]
-        if key not in cloth:
-            cloth[key] = 1
+        if i[1] not in cloth:
+            cloth[i[1]] = 1
         else:
-            cloth[key] += 1
-
-    for i in len(cloth.values()):
+            cloth[i[1]] += 1
+    for i in cloth.values():
         answer *= (i+1)
     return answer - 1
-
