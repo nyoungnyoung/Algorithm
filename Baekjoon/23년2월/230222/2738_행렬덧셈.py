6f9cdfdb05d75d82d698a619be14e745d56cd79f
yunseonyoung <sunysa8@naver.com>
2023-02-22 12:54:04 +0900

    백준-2744.대소문자바꾸기

--- Baekjoon/23년2월/230222/2738_행렬덧셈.py
+++ Baekjoon/23년2월/230222/2738_행렬덧셈.py
@@ -4,10 +4,10 @@ A = [list(map(int, sys.stdin.readline().split())) for _ in range(N)]
 B = [list(map(int, sys.stdin.readline().split())) for _ in range(N)]
 result = [[0] * M for _ in range(N)]
 for i in range(N):
-    if i != 0: print()
     for j in range(M):
         print(A[i][j] + B[i][j], end=" ") 
-        
+    print()
+    
 # 다른사람 코드
 # N, M = map(int, input().split())
 # p=[]
---
+++ Baekjoon/23년2월/230222/2744_대소문자바꾸기.py
@@ -0,0 +1,9 @@
+word = input()
+newWord = ''
+for i in word:
+    if i.isupper():
+        newWord += i.lower()
+    else:
+        newWord += i.upper()
+
+print(newWord)
