7a17ed6b9ca314df1fe566c0a845cddb739149fb
sunysa8 <sunysa8@naver.com>
2023-07-14 03:16:03 +0900

    백준-24480.알고리즘수업_깊이우선탐색2

---
+++ Baekjoon/23년7월/230713/24480_알고리즘수업_DFS2.py
@@ -0,0 +1,29 @@
+import sys
+from collections import defaultdict
+sys.setrecursionlimit(100000)
+
+def dfs(s):
+    global num
+    visited[s] = num            # 시작지점 방문처리
+    for i in sorted(graph[s], reverse=True):  # 시작지점과 이어진 노드 내림차순으로 탐색
+        if not visited[i]:      # 방문한적 없으면
+            num += 1            # num에 +1
+            dfs(i)              # dfs 수행
+
+N, M, R = map(int, input().split())
+graph = defaultdict(list)
+visited = [0] * (N + 1)
+num = 1
+
+# 인접 리스트 생성
+for _ in range(M):
+    u, v = map(int, input().split())
+    graph[u].append(v)
+    graph[v].append(u)
+
+# R번부터 시작해서 DFS 탐색
+dfs(R)
+
+# 1번부터 N번 노드까지 방문순서 출력
+for i in range(1, N + 1):
+    print(visited[i])
