a4960702f549532d1789badd3aab9dce8654acda
sunysa8 <sunysa8@naver.com>
2023-09-13 13:22:05 +0900

    백준-2156.포도주시식

---
+++ Baekjoon/23년9월/230913/2156_포도주시식.py
@@ -0,0 +1,21 @@
+import sys
+
+input = sys.stdin.readline
+
+N = int(input().strip())
+wine = [int(input().strip()) for _ in range(N)]
+
+# 마실 수 있는 최대 포도주 양을 저장해 줄 dp
+dp = [0] * N
+dp[0] = wine[0]
+dp[1] = wine[0] + wine[1]
+dp[2] = max(dp[1], wine[0] + wine[2], wine[1] + wine[2])
+
+# 연속으로 놓여진 3잔을 모두 마실 수는 없음
+# 현재 포도주 O, 이전 포도주 O, 전전 포도주 X
+# 현재 포도주 O, 이전 포도주 X, 전전 포도주 O
+# 현재 포도주 X
+for i in range(3, N):
+    dp[i] = max(wine[i] + wine[i - 1] + dp[i - 3], wine[i] + dp[i - 2], dp[i - 1])
+
+print(dp[N - 1])
